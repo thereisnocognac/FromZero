a1aacba6d7975b789ab698eb711110fa9e87e5a9
thereisnocognac <yollovesxyra@gmail.com>
2023-06-09 04:08:31 -0400

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MainBanner from './banner'
 
 function App() {
   return (
-    <div className='flex flex-col justify-center content-center'>
+    <div>
     <MainBanner />
     </div>
   )
--- src/banner.jsx
+++ src/banner.jsx
@@ -4,7 +4,7 @@ import fft from './assets/fftp.png';
 export default function MainBanner() {
     return (
         <div className="flex flex-col">
-        <img src={fft} alt=""  className='mt-auto'/>
+        <img src={fft} alt=""  className=''/>
         <input type="text"  className='bg-white text-black'/>
         </div>
     )
