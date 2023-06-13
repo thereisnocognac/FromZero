a536c544e24cd4f5b3365364722fb305fd0fa832
thereisnocognac <yollovesxyra@gmail.com>
2023-06-13 01:24:32 -0400

    added vars

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -1,5 +1,11 @@
 let btnHandle = document.querySelector('#input-btn');
+let myLeads = [];
+const inputEl = document.querySelector('#input-el');
+
+
+
 btnHandle.addEventListener("click", function() {
     console.log("Button clicked from addEventListener")
 });
+
 
