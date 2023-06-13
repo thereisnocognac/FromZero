d0795999cc24be275f2e1da5fc4da8f89b464176
thereisnocognac <yollovesxyra@gmail.com>
2023-06-12 22:15:52 -0400

    yay

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -1,5 +1,5 @@
 let btnHandle = document.querySelector('#input-btn');
+btnHandle.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+});
 
-function saveLead() {
-    console.log("Button clicked!")
-}
