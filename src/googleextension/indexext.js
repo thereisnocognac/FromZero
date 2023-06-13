a2e17a431f3f0fdff7ca55354633cdec8b6d473f
thereisnocognac <yollovesxyra@gmail.com>
2023-06-13 17:34:18 -0400

    added

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -1,20 +1,20 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 let inputValue = document.querySelector('#input-el').value;
 const inputEl = document.querySelector('#input-el');
 const btnHandle = document.querySelector('#input-btn');
 const ulEl = document.querySelector('#ul-el');
-let listItems = '';
+
 
 
 
 btnHandle.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads)
     renderLeads()
 });
 
 
 function renderLeads() {
+    let listItems = '';
     for (let i = 0; i < myLeads.length; i++) {
         listItems += "<li>" + myLeads[i] + "</li>";
     }
