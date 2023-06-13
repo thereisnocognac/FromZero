2746dffacdb682d42c2708c6e14f36b3141a031f
thereisnocognac <yollovesxyra@gmail.com>
2023-06-13 01:47:23 -0400

    yes

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -1,11 +1,12 @@
 let myLeads = [];
+let inputValue = document.querySelector('#input-el').value
 const inputEl = document.querySelector('#input-el');
 const btnHandle = document.querySelector('#input-btn');
 
 
 
 btnHandle.addEventListener("click", function() {
-    myLeads.push('www.awesomelead.com');
+    myLeads.push(inputValue);
     console.log(myLeads)
 });
 
