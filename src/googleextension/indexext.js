d8e71b51564f1e87703105fd0996404f4de7280c
thereisnocognac <yollovesxyra@gmail.com>
2023-06-13 02:22:34 -0400

    added ul

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -2,6 +2,7 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 let inputValue = document.querySelector('#input-el').value;
 const inputEl = document.querySelector('#input-el');
 const btnHandle = document.querySelector('#input-btn');
+const ulEl = document.querySelector('#ul-el');
 
 
 
