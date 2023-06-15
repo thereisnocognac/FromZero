fa51c99912567d1d6c6537a1b14edacd98450693
thereisnocognac <yollovesxyra@gmail.com>
2023-06-14 20:13:07 -0400

    nay

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -1,39 +1,31 @@
-let myLeads = [];
-let inputValue = document.querySelector('#input-el').value;
-const inputEl = document.querySelector('#input-el');
-const btnHandle = document.querySelector('#input-btn');
-const ulEl = document.querySelector('#ul-el');
-
-
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    myLeads = renderLeads();
+    myLeads = leadsFromLocalStorage
+    renderLeads()
 }
 
-
-// localStorage.setItem('myLeads', 'www.example.com');
-
-
-btnHandle.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = '';
-    localStorage.setItem('myLeads', JSON.stringify(myLeads) );
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
-});
-
+})
 
 function renderLeads() {
-    let listItems = '';
+    let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='https://${myLeads[i]}'>
+                <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems  
 }
