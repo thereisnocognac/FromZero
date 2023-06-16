491627701ce5100387c8c2185c7d765373b73392
thereisnocognac <yollovesxyra@gmail.com>
2023-06-15 23:45:24 -0400

    yes

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -35,24 +35,17 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
 
 tabBtn.addEventListener("click", function () {
 
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 
- 
-
-  });
-
-
-
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    
 })
 
 deleteBtn.addEventListener("dblclick", function() {
