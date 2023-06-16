e98d5b87a3a61534c7807e014abe95a3a2de3c40
thereisnocognac <yollovesxyra@gmail.com>
2023-06-15 23:13:50 -0400

    added tab

--- src/googleextension/indexext.js
+++ src/googleextension/indexext.js
@@ -41,7 +41,9 @@ const tabs = [
 
 
 tabBtn.addEventListener("click", function () {
-    console.log(tabs[0].url)
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
 deleteBtn.addEventListener("dblclick", function() {
